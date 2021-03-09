17b5a45046d3e34d23793492dc9e602f6bb03c1b
derisen <dogan.erisen@gmail.com>
2021-03-09 15:26:29 -0800

    c2s1: add id token claims

--- 2-Authorization-I/1-call-graph/SPA/src/app/home/home.component.ts
+++ 2-Authorization-I/1-call-graph/SPA/src/app/home/home.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { filter, takeUntil } from 'rxjs/operators';
+
 import { MsalBroadcastService, MsalService } from '@azure/msal-angular';
 import { EventMessage, EventType } from '@azure/msal-browser';
-import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -9,7 +11,12 @@ import { filter } from 'rxjs/operators';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+
   loginDisplay = false;
+  displayedColumns: string[] = ['claim', 'value'];
+  dataSource: any =[];
+
+  private readonly _destroying$ = new Subject<void>();
 
   constructor(private authService: MsalService, private msalBroadcastService: MsalBroadcastService) { }
 
@@ -17,19 +24,48 @@ export class HomeComponent implements OnInit {
     this.msalBroadcastService.msalSubject$
       .pipe(
         filter((msg: EventMessage) => msg.eventType === EventType.LOGIN_SUCCESS),
+        takeUntil(this._destroying$)
       )
       .subscribe((result: EventMessage) => {
-        console.log(result);
         if (result?.payload?.account) {
-          this.authService.instance.setActiveAccount(result.payload.account);
+          this.authService.instance.setActiveAccount(result?.payload?.account);
         }
       });
 
       this.setLoginDisplay();
+      this.checkAndSetActiveAccount();
+      this.getClaims(this.authService.instance.getActiveAccount()?.idTokenClaims)
+
+  }
+
+  checkAndSetActiveAccount() {
+    /**
+     * If no active account set but there are accounts signed in, sets first account to active account
+     * To use active account set here, subscribe to inProgress$ first in your component
+     * Note: Basic usage demonstrated. Your app may require more complicated account selection logic
+     */
+    let activeAccount = this.authService.instance.getActiveAccount();
+
+    if (!activeAccount && this.authService.instance.getAllAccounts().length > 0) {
+      let accounts = this.authService.instance.getAllAccounts();
+      this.authService.instance.setActiveAccount(accounts[0]);
+    }
   }
 
   setLoginDisplay() {
     this.loginDisplay = this.authService.instance.getAllAccounts().length > 0;
   }
 
+  getClaims(claims: any) {
+    this.dataSource = [
+      {id: 1, claim: "Display Name", value: claims ? claims['name'] : null},
+      {id: 2, claim: "User Preferred Name (UPN)", value: claims ? claims['preferred_username'] : null},
+      {id: 2, claim: "OID", value: claims ? claims['oid']: null}
+    ];
+  }
+
+  ngOnDestroy(): void {
+    this._destroying$.next(undefined);
+    this._destroying$.complete();
+  }
 }
