58d185f22cd2f0a0f872e24ac95fd43af5065577
derisen <dogan.erisen@gmail.com>
2021-08-04 23:13:22 -0700

    fix path issue, karma config

--- 5-AccessControl/2-call-api-groups/SPA/karma.conf.js
+++ 5-AccessControl/2-call-api-groups/SPA/karma.conf.js
@@ -28,7 +28,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadlessCI'],
     singleRun: false,
     restartOnFileChange: true
   });
