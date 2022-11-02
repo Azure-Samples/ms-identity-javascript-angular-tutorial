89c7f3036f6b550dc63688dfb7fed2d25a78d802
Doğan Erişen <dogan.erisen@gmail.com>
2022-11-01 17:40:48 -0700

    add user flows

--- 1-Authentication/2-sign-in-b2c/SPA/src/app/app.component.ts
+++ 1-Authentication/2-sign-in-b2c/SPA/src/app/app.component.ts
@@ -1,8 +1,16 @@
 import { Component, OnInit, Inject, OnDestroy } from '@angular/core';
-import { MsalService, MsalBroadcastService, MSAL_GUARD_CONFIG, MsalGuardConfiguration } from '@azure/msal-angular';
-import { AuthenticationResult, EventMessage, EventType, InteractionStatus, InteractionType, PopupRequest, RedirectRequest } from '@azure/msal-browser';
 import { Subject } from 'rxjs';
 import { filter, takeUntil } from 'rxjs/operators';
+
+import { IdTokenClaims, PromptValue } from '@azure/msal-common';
+import { AccountInfo, AuthenticationResult, EventMessage, EventType, InteractionStatus, InteractionType, PopupRequest, RedirectRequest, SsoSilentRequest } from '@azure/msal-browser';
+import { MsalService, MsalBroadcastService, MSAL_GUARD_CONFIG, MsalGuardConfiguration } from '@azure/msal-angular';
+import { b2cPolicies } from './auth-config';
+
+type IdTokenClaimsWithPolicyId = IdTokenClaims & {
+    acr?: string,
+    tfp?: string,
+};
 
 @Component({
     selector: 'app-root',
@@ -31,7 +39,6 @@ export class AppComponent implements OnInit, OnDestroy {
          * You can subscribe to MSAL events as shown below. For more info,
          * visit: https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-angular/docs/v2-docs/events.md
          */
-
         this.msalBroadcastService.msalSubject$
             .pipe(
                 filter((msg: EventMessage) => msg.eventType === EventType.ACCOUNT_ADDED || msg.eventType === EventType.ACCOUNT_REMOVED),
@@ -53,6 +60,85 @@ export class AppComponent implements OnInit, OnDestroy {
                 this.setLoginDisplay();
                 this.checkAndSetActiveAccount();
             })
+
+        this.msalBroadcastService.msalSubject$
+            .pipe(
+                filter((msg: EventMessage) => msg.eventType === EventType.LOGIN_SUCCESS
+                    || msg.eventType === EventType.ACQUIRE_TOKEN_SUCCESS
+                    || msg.eventType === EventType.SSO_SILENT_SUCCESS),
+                takeUntil(this._destroying$)
+            )
+            .subscribe((result: EventMessage) => {
+
+                let payload = result.payload as AuthenticationResult;
+                let idtoken = payload.idTokenClaims as IdTokenClaimsWithPolicyId;
+
+                if (idtoken.acr === b2cPolicies.names.signUpSignIn || idtoken.tfp === b2cPolicies.names.signUpSignIn) {
+                    this.authService.instance.setActiveAccount(payload.account);
+                }
+
+                /**
+                 * For the purpose of setting an active account for UI update, we want to consider only the auth response resulting
+                 * from SUSI flow. "acr" claim in the id token tells us the policy (NOTE: newer policies may use the "tfp" claim instead).
+                 * To learn more about B2C tokens, visit https://docs.microsoft.com/en-us/azure/active-directory-b2c/tokens-overview
+                 */
+                if (idtoken.acr === b2cPolicies.names.editProfile || idtoken.tfp === b2cPolicies.names.editProfile) {
+
+                    // retrieve the account from initial sing-in to the app
+                    const originalSignInAccount = this.authService.instance.getAllAccounts()
+                        .find((account: AccountInfo) =>
+                            account.idTokenClaims?.oid === idtoken.oid
+                            && account.idTokenClaims?.sub === idtoken.sub
+                            && ((account.idTokenClaims as IdTokenClaimsWithPolicyId).acr === b2cPolicies.names.signUpSignIn
+                                || (account.idTokenClaims as IdTokenClaimsWithPolicyId).tfp === b2cPolicies.names.signUpSignIn)
+                        );
+
+                    let signUpSignInFlowRequest: SsoSilentRequest = {
+                        authority: b2cPolicies.authorities.signUpSignIn.authority,
+                        account: originalSignInAccount
+                    };
+
+                    // silently login again with the signUpSignIn policy
+                    this.authService.ssoSilent(signUpSignInFlowRequest);
+                }
+
+                /**
+                 * Below we are checking if the user is returning from the reset password flow.
+                 * If so, we will ask the user to reauthenticate with their new password.
+                 * If you do not want this behavior and prefer your users to stay signed in instead,
+                 * you can replace the code below with the same pattern used for handling the return from
+                 * profile edit flow (see above ln. 74-92).
+                 */
+                if (idtoken.acr === b2cPolicies.names.resetPassword || idtoken.tfp === b2cPolicies.names.resetPassword) {
+                    let signUpSignInFlowRequest: RedirectRequest | PopupRequest = {
+                        authority: b2cPolicies.authorities.signUpSignIn.authority,
+                        prompt: PromptValue.LOGIN, // force user to reauthenticate with their new password
+                        scopes: []
+                    };
+
+                    this.login(signUpSignInFlowRequest);
+                }
+
+                return result;
+            });
+
+        this.msalBroadcastService.msalSubject$
+            .pipe(
+                filter((msg: EventMessage) => msg.eventType === EventType.LOGIN_FAILURE || msg.eventType === EventType.ACQUIRE_TOKEN_FAILURE),
+                takeUntil(this._destroying$)
+            )
+            .subscribe((result: EventMessage) => {
+                // Checking for the forgot password error. Learn more about B2C error codes at
+                // https://learn.microsoft.com/azure/active-directory-b2c/error-codes
+                if (result.error && result.error.message.indexOf('AADB2C90118') > -1) {
+                    let resetPasswordFlowRequest: RedirectRequest | PopupRequest = {
+                        authority: b2cPolicies.authorities.resetPassword.authority,
+                        scopes: [],
+                    };
+
+                    this.login(resetPasswordFlowRequest);
+                };
+            });
     }
 
     setLoginDisplay() {
@@ -74,30 +160,39 @@ export class AppComponent implements OnInit, OnDestroy {
         }
     }
 
-    login() {
+    login(userFlowRequest?: RedirectRequest | PopupRequest) {
         if (this.msalGuardConfig.interactionType === InteractionType.Popup) {
             if (this.msalGuardConfig.authRequest) {
-                this.authService.loginPopup({ ...this.msalGuardConfig.authRequest } as PopupRequest)
+                this.authService.loginPopup({ ...this.msalGuardConfig.authRequest, ...userFlowRequest } as PopupRequest)
                     .subscribe((response: AuthenticationResult) => {
                         this.authService.instance.setActiveAccount(response.account);
                     });
             } else {
-                this.authService.loginPopup()
+                this.authService.loginPopup(userFlowRequest)
                     .subscribe((response: AuthenticationResult) => {
                         this.authService.instance.setActiveAccount(response.account);
                     });
             }
         } else {
             if (this.msalGuardConfig.authRequest) {
-                this.authService.loginRedirect({ ...this.msalGuardConfig.authRequest } as RedirectRequest);
+                this.authService.loginRedirect({ ...this.msalGuardConfig.authRequest, ...userFlowRequest } as RedirectRequest);
             } else {
-                this.authService.loginRedirect();
+                this.authService.loginRedirect(userFlowRequest);
             }
         }
     }
 
     logout() {
         this.authService.logout();
+    }
+
+    editProfile() {
+        let editProfileFlowRequest: RedirectRequest | PopupRequest = {
+            authority: b2cPolicies.authorities.editProfile.authority,
+            scopes: [],
+        };
+
+        this.login(editProfileFlowRequest);
     }
 
     // unsubscribe to events when component is destroyed
--- 1-Authentication/2-sign-in-b2c/SPA/src/app/auth-config.ts
+++ 1-Authentication/2-sign-in-b2c/SPA/src/app/auth-config.ts
@@ -16,18 +16,22 @@ const isIE = window.navigator.userAgent.indexOf("MSIE ") > -1 || window.navigato
  */
 export const b2cPolicies = {
     names: {
-        signUpSignIn: "b2c_1_susi_reset_v2",
-        editProfile: "b2c_1_edit_profile_v2"
+        signUpSignIn: 'B2C_1_susi_v2',
+        resetPassword: 'B2C_1_reset_v3',
+        editProfile: 'B2C_1_edit_profile_v2',
     },
     authorities: {
         signUpSignIn: {
-            authority: "https://fabrikamb2c.b2clogin.com/fabrikamb2c.onmicrosoft.com/b2c_1_susi_reset_v2",
+            authority: 'https://fabrikamb2c.b2clogin.com/fabrikamb2c.onmicrosoft.com/b2c_1_susi_v2',
+        },
+        resetPassword: {
+            authority: 'https://fabrikamb2c.b2clogin.com/fabrikamb2c.onmicrosoft.com/B2C_1_reset_v3',
         },
         editProfile: {
-            authority: "https://fabrikamb2c.b2clogin.com/fabrikamb2c.onmicrosoft.com/b2c_1_edit_profile_v2"
-        }
+            authority: 'https://fabrikamb2c.b2clogin.com/fabrikamb2c.onmicrosoft.com/b2c_1_edit_profile_v2',
+        },
     },
-    authorityDomain: "fabrikamb2c.b2clogin.com"
+    authorityDomain: 'fabrikamb2c.b2clogin.com',
 };
 
 /**
