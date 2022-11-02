ed77a673af30ccd2f9734a88d234dc0eeb1561a9
Doğan Erişen <dogan.erisen@gmail.com>
2022-11-01 17:02:00 -0700

    fix redirect uri mismatch in 3-1

--- 3-Authorization-II/1-call-api/AppCreationScripts/Configure.ps1
+++ 3-Authorization-II/1-call-api/AppCreationScripts/Configure.ps1
@@ -276,7 +276,7 @@ Function ConfigureApplications
    $clientAadApplication = New-MgApplication -DisplayName "msal-angular-spa" `
                                                       -Spa `
                                                       @{ `
-                                                          RedirectUris = "http://localhost:4200"; `
+                                                          RedirectUris = "http://localhost:4200/", "http://localhost:4200/auth"; `
                                                         } `
                                                        -SignInAudience AzureADMyOrg `
                                                       #end of command
