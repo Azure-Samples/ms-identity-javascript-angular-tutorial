45998f9d7f0bbb70bd7ad18d627db6855e73a1bd
Brian Melton-Grace <brianmel@microsoft.com>
2024-05-17 09:52:15 -0700

    Archive repo

--- 6-AdvancedScenarios/1-call-api-obo/SPA/src/app/profile.ts
+++
@@ -1,5 +0,0 @@
-import { User } from "@microsoft/microsoft-graph-types";
-
-export type Profile = Pick<User, "id" | "userPrincipalName" | "givenName" | "surname" | "jobTitle" | "mobilePhone" | "preferredLanguage"> & {
-    firstLogin: boolean,
-}
